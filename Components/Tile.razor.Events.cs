09eee3b6d9517f850fd0e3735521edef20ca0122
Hershey Ann Estoya <hestoya@thurstonsoft.com>
2026-05-27 12:17:23 -0700

    v2-file_structure: Update File Structure

---
+++ Components/Tile.razor.Events.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+
+namespace ColorPaletteGenerator.Components
+{
+    public partial class Tile
+    {
+        private async Task CopyColorToClipboard()
+        {
+            await JS.InvokeVoidAsync("navigator.clipboard.writeText", HexValue);
+
+            tooltipText = "Copied!";
+            StateHasChanged();
+
+            await Task.Delay(1200);
+
+            tooltipText = "Copy";
+            StateHasChanged();
+        }
+    }
+}
